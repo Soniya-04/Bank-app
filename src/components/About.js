051d33b7bcd7b32a01179fcdb0d53df853bf185b
Soniya-04 <soniyagunduraj02@gmail.com>
2025-03-16 01:20:29 +0530

    BA-2 Created About.js and About.css

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
       <h2>About Us</h2>
       <p>We are a modern banking service, offering a variety of financial services.</p>
     </div>
-  );
+  );   
 }
 
 export default About;
